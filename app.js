2799e3d709efa9ca82c1f5c75ae21ecb4c6ecb06
FatimaaTA <fatima.khaliqdad656@gmail.com>
2020-06-14 18:16:03 +0500

    practice

---
+++ app.js
@@ -0,0 +1,306 @@
+/* ******************Chapter 1******************
+
+ ------Question 1------*/
+// alert("Welcome! For checking my Assignment");
+
+//------Question 2------
+// alert("Error! Please enter a valid password.");
+
+//------Question 3------
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+//------Question 4------
+//alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+//prevent this page from creating additional dailogue boxes is a browser feature
+
+//------Question 5------
+//Done using console.console option opens from inspecting site.
+
+//------Question 6------
+//no app.js file is connected function is written inside script in html file
+// <body onload="showAlert()">
+    
+// </body >
+// <script >
+//     function showAlert() {
+//     alert ("Hello world!");
+//   }
+// </script>
+
+//------Question 7------
+//Scripts works on all (inside header,before body,inside body and after body) best practice 
+//is to use after body tag so that user interface loads first and then functionality 
+
+/* ******************Chapter 2******************
+------Question 1------*/
+// var username;
+
+//------Question 2------
+// var myName = "Fatima Khaliqdad";
+
+//------Question 3------
+// var message;
+// message = "Hello World!";
+// alert(message);
+
+//------Question 4------
+// var name = "Johne Doe";
+// var age = "15 years old";
+// var education = "Certified Mobile Application Development" ;
+// alert(name);
+// alert(age);
+// alert(education);
+
+//------Question 5------
+// var display = "PIZZA";
+// alert(display.slice(0,5)+"\n"+display.slice(0,4)+"\n"+
+// display.slice(0,3)+"\n"+display.slice(0,2)+"\n"+display.slice(0,1)+"\n");
+
+//------Question 6------
+// var email = "email@example.com";
+// alert("My email address is "+email);
+
+//------Question 7------
+// var book = "A smarter \n way to learn JavaScript";
+// alert("I am trying to learn from the Book "+book);
+
+//------Question 8------
+// document.write("Yah! I can write HTML content through JavaScript");
+
+//------Question 9------
+// var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert("                "+string);
+
+/* ******************Chapter 3******************
+
+ ------Question 1------*/
+// var age = 25;
+// alert("I am "+age+" years old");
+
+// ------Question 2------
+// var visit = 14;
+// alert("You have visited the site "+visit+" times");
+
+// ------Question 3------
+// var birthYear = 1990;
+// document.write("My birth year is "+birthYear+" <br> Data type of my declared cariable is number");
+
+// ------Question 4------
+// var name = "Fatima";
+// var title = "T-Shirt(s)";
+// var quantity = 5;
+// document.write(name+" ordered "+quantity+" "+title+" on XYZ Clothing store");
+
+/* ******************Chapter 4******************
+
+ ------Question 1------*/
+// var name,title,quantity;
+
+// ------Question 2------
+// var age, firstName, age4 ,$last_Name, full$Name;
+
+// ------Question 3------
+// var 4age, first Name, @age ,var, *full$Name;
+
+// ------Question 4------
+// document.write("<h1>Rules for naming JS variables</h1><br>"+
+// "b) Variable names can only contain <b><u>letter</u></b> , <b><u>number</u></b> , <b>$</b> and<b> _</b>. For example <b>$my_1stVariable</b><br>"+
+// "c) Variables must begin with a <b><u>letter</u></b> ,<b> $</b> or <b>_</b>. For example <b>$name, _name</b> or <b>name</b><br>"+
+// "d) Variable names are case <b><u>Sensitive</u></b><br>"+
+// "e) Variable names should not be JS <b><u>keywords</u></b>");
+
+/* ******************Chapter 5******************
+
+ ------Question 1------*/
+//  var firstNumber = 5;
+//  var secondNumber = 3;
+//  var sum = firstNumber+secondNumber;
+//  document.write("Sum of "+firstNumber+" and "+secondNumber+" is "+sum);
+
+// ------Question 2------
+//Subtraction of two numbers
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var sub = firstNumber-secondNumber;
+// document.write("Subtraction of "+firstNumber+" and "+secondNumber+" is "+sub+"<br>");
+// //Multiplication of two numbers
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var mul = firstNumber*secondNumber;
+// document.write("Multiplication of "+firstNumber+" and "+secondNumber+" is "+mul+"<br>");
+// //Modulus of two numbers
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var mod = firstNumber%secondNumber;
+// document.write("Modulus of "+firstNumber+" and "+secondNumber+" is "+mod+"<br>");
+
+// ------Question 3------
+// var number;
+// document.write("Value after variable declaration is "+number+"<br>");
+// number = 5;
+// document.write("Intial value: "+number+"<br>");
+// number++;
+// document.write("Value after increment is "+number+"<br>");
+// number+=7;
+// document.write("Value after addition is "+number+"<br>");
+// number--;
+// document.write("Value after decrement is "+number+"<br>");
+// number%=3;
+// document.write("The remainder is "+number+"<br>");
+
+// ------Question 4------
+// var costOfTicket = 600;
+// var costOf5Tickets = costOfTicket*5;
+// document.write("Total cost to buy 5 tickets to a movie is "+costOf5Tickets+"PKR");
+
+// ------Question 5------
+// var num = 4, num1 = 1;
+// document.write("Table of "+num+"<br>"+num+" X "+num1+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>");
+
+// ------Question 6------
+// var  celsiusTemperature = 25, fahrenheitTemperature = 70;
+// var convertToFahrenheit = (celsiusTemperature*(9/5))+32;
+// var convertToCelsius = (fahrenheitTemperature-32)*(5/9);
+// document.write(celsiusTemperature+"°C is "+convertToFahrenheit+"°F"+"<br>");
+// document.write(fahrenheitTemperature+"°F is "+convertToCelsius+"°C"+"<br>");
+
+// ------Question 7------
+// var costOfItem1 = 650, costOfItem2 = 100, quantityOfItem1 = 3, quantityOfItem2 = 7 , shippingCharges = 100, totalCost;
+// totalCost = (costOfItem1*quantityOfItem1)+(costOfItem2*quantityOfItem2)+shippingCharges;
+// document.write("<h1>Shopping Cart</h1>"+"Price of item 1 is "+costOfItem1+"<br>"+"Quantity of item 1 is "+quantityOfItem1+"<br>"+
+// "Price of item 2 is "+costOfItem2+"<br>"+"Quantity of item 2 is "+quantityOfItem2+"<br>"+"Shipping Charges "+shippingCharges+"<br><br>"+
+// "Total cost of your order is "+totalCost);
+
+// ------Question 8------
+// var totalMarks = 980, obtainedMarks = 804;
+// document.write("<h1>Marks Sheet</h1><br>"+"Total Marks : "+totalMarks+"<br>Marks Obtained : "+obtainedMarks+
+// "<br>Percentage : "+((obtainedMarks/totalMarks)*100));
+
+// ------Question 9------
+// var usDollar = 10, saudiRiyal = 25 ,$ToPKR = 104.80, riyalToPKR = 28, totalCurrency;
+// totalCurrency = (usDollar*$ToPKR)+(saudiRiyal*riyalToPKR);
+// document.write("<h1>Curresncy in PKR</h1><br>"+"Total currency in PKR : "+totalCurrency);
+
+// ------Question 10------
+// var num = 5;
+// var result = ((num+5)*10)/2
+// document.write("Number is : "+num+"<br>a. Add 5<br>b. Multiply by 10 <br>c. Divide the result by 2<br>Result : "+result);
+
+// ------Question 11------
+// var currentYear = 2020, birthYear = 1995, age;
+// age = currentYear-birthYear;
+// document.write("<h1>Age Calculator</h1><br>Current Year : "+currentYear+"<br> Birth Year : "+birthYear+"<br>Your age : "+age+" or "+--age);
+
+// ------Question 12------
+// var radius = 20, pi = 3.142, circumference, area;
+// circumference = 2*pi*radius;
+// area = pi*(Math.pow(radius,2));
+// document.write("<h1>The Geometrizer</h1><br> Radius of a circle : "+radius+"<br>Circumferece of a circle : "+circumference+
+// "<br>Area of a circle : "+area);
+
+// ------Question 13------
+//This question is solved wrong in practice exercise ************Important***********
+// var currentage = 15, favSnack = "Chips", maxAge = 65, snackPerDay = 3;
+// var SnackTillMaxAge = (snackPerDay*365)*(maxAge-currentage);
+// document.write("<h1>Lifetime Supply Calculator</h1><br>Favourite Snack : "+favSnack+"<br>Current Age : "+currentage+
+// "<br>Estimated max age : "+maxAge+"<br>Amount of snacks per day : "+snackPerDay+"<br>You will need "+
+// SnackTillMaxAge+" to last you until the ripe old age of "+maxAge);
+
+/* ******************Chapter 6******************
+
+ ------Question 1------*/
+// var a = 10;
+// document.write("Result<br> The value of a is : "+a+"<br>.........................................<br><br>"+
+// "The value of ++a is : "+(++a)+"<br>Now the value of a is : "+a+"<br><br>"+
+// "The value of a++ is : "+(a++)+"<br>Now the value of a is : "+a+"<br><br>"+
+// "The value of --a is : "+(--a)+"<br>Now the value of a is : "+a+"<br><br>"+
+// "The value of a-- is : "+(a--)+"<br>Now the value of a is : "+a+"<br><br>");
+
+// ------Question 2------
+// var a = 2, b = 1;
+// document.write("a is : "+a+"<br>b is : "+b+"<br>");
+// var result = --a - --b + ++b + b--;
+// //1-0+1+1
+// document.write("--a - --b + ++b + b--<br>//1-0+1+1<br>result is : "+result);
+
+// ------Question 3------
+// var username = prompt("Your name? ");
+// document.write("Welcome! "+username);
+
+// ------Question 4 and 5------
+// var num = prompt("Enter a Number?","5"), num1 = 1;
+// document.write("Table of "+num+"<br>"+num+" X "+num1+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>"+
+// num+" X "+(++num1)+" = "+(num*num1)+"<br>");
+
+// ------Question 6------
+// var subjects=prompt("Enter 3 subjects name?","Math  ,  Phys  ,  Urdu"),
+//      firstSubject = subjects.split(",")[0],
+//      secondSubject = subjects.split(",")[1],
+//      thirdSubject = subjects.split(",")[2];
+// var obtainedMarks = prompt("Enter 3 subjects number?","50  ,  68  ,  82"),
+//      firstSubjectNo = parseInt(obtainedMarks.split(",")[0]),
+//      secondSubjectNo = parseInt(obtainedMarks.split(",")[1]),
+//      thirdSubjectNo = parseInt(obtainedMarks.split(",")[2]);
+//      var totalmarks = 300;
+//      var totalOf1Subject = 100;
+//      var totalObtOfAllSub = firstSubjectNo+secondSubjectNo+thirdSubjectNo;
+//      var percentOf1stSubject = (firstSubjectNo/totalOf1Subject)*100;
+//      var percentOf2ndSubject = (secondSubjectNo/totalOf1Subject)*100;
+//      var percentOf3rdSubject = (thirdSubjectNo/totalOf1Subject)*100;
+//      var totalPercentage = parseInt((totalObtOfAllSub/(totalmarks))*100);
+//      var space = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+//      var space1 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+//      document.write("Subjects    Total Marks    Obtained Marks    Percentage<br>"+
+//      firstSubject+space+totalOf1Subject+space+firstSubjectNo+space+percentOf1stSubject+"%<br>"+
+//      secondSubject+space+totalOf1Subject+space+secondSubjectNo+space+percentOf2ndSubject+"%<br>"+
+//      thirdSubject+space+totalOf1Subject+space+thirdSubjectNo+space+percentOf3rdSubject+"%<br>"+
+//      space1+totalmarks+space+totalObtOfAllSub+space+totalPercentage+"%");
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
